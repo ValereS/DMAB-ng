64c05db2496d8c0f9c377ebea0194497c74c81e0
My Name <me@forme.com>
2018-01-28 16:48:14 +0100

    navigation-bar, bootstrap, first stub: members

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 
 
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
