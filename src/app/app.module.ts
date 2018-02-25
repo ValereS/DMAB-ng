3b37c946128d4e2432eee293615c9d41f9b2d15f
val <me@forme.com>
2018-02-25 20:05:09 +0100

    revmove this shitty carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CoreModule } from './core/core.module';
 
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { CarouselModule } from 'primeng/carousel';
+
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { CarouselComponent } from './home/carousel/carousel.component';
-import { CarouselService } from './home/carousel/service/carousel.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    CarouselComponent
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
-    CarouselModule,
-    HttpClientModule
+    CoreModule
   ],
-  providers: [
-    CarouselService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/carousel/i-article.ts
+++
@@ -1,9 +0,0 @@
-export interface IArticle {
-  id: string;
-  title: string;
-  creationDateT: Date;
-  summary: string;
-  image: string;
-  description: string;
-  keywords: string[];
-}
